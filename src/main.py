5d2bfc00383f0a8ac4b0b1c4b87e289391616a22
Nikhil Dave <nikhilrdave2003@gmail.com>
2024-03-23 08:50:45 -0700

    Changed main page to be display of stats for selected mirrors
    Users can select countries for mirrors to look at, statistics to view,
    and specific mirrors to look at.

--- src/main.py
+++ src/main.py
@@ -1,26 +1,43 @@
 import streamlit as st
 from requests import get
-import data
+from data import get_mirror_info
 
 
 def main():
     st.title("Arch Linux Mirrors")
     try:
-        mirror_data = data.get_mirror_info()
+        mirror_data = get_mirror_info()
 
-        fields = st.multiselect(
-            "Select data fields to display: ",
-            list(mirror_data[0].keys()),
-            default=["url", "score"],
+        country_set = sorted({m["country"] for m in mirror_data})
+        selected_countries = st.multiselect(
+            "Select Mirror Country/Countries: ", list(country_set)
         )
 
-        st.dataframe(
-            [
-                {key: val for key, val in m.items() if key in fields}
-                for m in mirror_data
-            ],
-            use_container_width=True,
+        valid_mirrors = [
+            m for m in mirror_data if m["country"] in selected_countries]
+
+        valid_keys = [
+            k
+            for k in mirror_data[0].keys()
+            if isinstance(mirror_data[0][k], (int, float))
+            and not isinstance(mirror_data[0][k], bool)
+        ]
+
+        selected_keys = st.multiselect(
+            "Select data values to compare", valid_keys)
+
+        selected_mirrors = st.multiselect(
+            "Select mirrors to compare",
+            valid_mirrors,
+            format_func=lambda m: m["url"],
         )
+
+        for key in selected_keys:
+            data = {m["url"]: m[key] for m in selected_mirrors}
+
+            st.subheader(key)
+            st.bar_chart(data)
+
     except ConnectionError:
         st.text("Failed to connect to archlinux.org")
 
