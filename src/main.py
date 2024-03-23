fbb1e64c5f1f867521a761130967ded5d700bce1
Nikhil Dave <nikhilrdave2003@gmail.com>
2024-03-22 20:15:10 -0700

    Cleaned up main page, added selection of statistics

--- src/main.py
+++ src/main.py
@@ -3,5 +3,22 @@ from requests import get
 import data
 
 
-st.title("Arch Linux Mirrors")
-st.table(data.get_mirror_info())
+def main():
+    st.title("Arch Linux Mirrors")
+    mirror_data = data.get_mirror_info()
+
+    fields = st.multiselect(
+        "Select data fields to display: ",
+        list(mirror_data[0].keys()),
+        default=["url", "score"],
+    )
+
+    st.dataframe(
+        [{key: val for key, val in m.items() if key in fields}
+         for m in mirror_data],
+        use_container_width=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
