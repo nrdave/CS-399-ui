11da111c00dc64c0483c22ccf4af8036fd2b518a
Nikhil Dave <nikhilrdave2003@gmail.com>
2024-03-22 20:44:43 -0700

    Moved main code into try-except to catch any raised errors from data

--- src/main.py
+++ src/main.py
@@ -5,19 +5,24 @@ import data
 
 def main():
     st.title("Arch Linux Mirrors")
-    mirror_data = data.get_mirror_info()
+    try:
+        mirror_data = data.get_mirror_info()
 
-    fields = st.multiselect(
-        "Select data fields to display: ",
-        list(mirror_data[0].keys()),
-        default=["url", "score"],
-    )
+        fields = st.multiselect(
+            "Select data fields to display: ",
+            list(mirror_data[0].keys()),
+            default=["url", "score"],
+        )
 
-    st.dataframe(
-        [{key: val for key, val in m.items() if key in fields}
-         for m in mirror_data],
-        use_container_width=True,
-    )
+        st.dataframe(
+            [
+                {key: val for key, val in m.items() if key in fields}
+                for m in mirror_data
+            ],
+            use_container_width=True,
+        )
+    except ConnectionError:
+        st.text("Failed to connect to archlinux.org")
 
 
 if __name__ == "__main__":
