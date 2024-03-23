e8a954c49439c66cbd288035c4c888259e89b9d8
Nikhil Dave <nikhilrdave2003@gmail.com>
2024-03-22 20:12:56 -0700

    Add error check, validation, and filtering to data retrieval
    get_mirror_data now filters out mirrors with a completion percentage of
    zero or no score.
    
    It also will display an error if an OSError occurs

--- src/data.py
+++ src/data.py
@@ -2,11 +2,30 @@ import streamlit as st
 from requests import get
 
 
+# Arch Linux mirror checks generally run around every 7 minutes
+@st.cache_data(show_spinner="Fetching data from archlinux.org", ttl=60 * 7)
 def get_mirror_info():
+    """Fetch current mirror data from archlinux.org/mirrors/status/json"""
     MIRROR_STATUS_URL = "https://archlinux.org/mirrors/status/json/"
 
-    mirror_status = get(MIRROR_STATUS_URL, timeout=5).json()
+    try:
+        # Get mirror info
+        mirror_status = get(MIRROR_STATUS_URL, timeout=5).json()
 
-    mirrors = mirror_status.get("urls")
+        # Extract information about each mirror
+        mirrors = mirror_status.get("urls")
 
-    return mirrors
+        # Filter out mirrors with either a score of Null or completion_pct of
+        # zero. These mirrors are likely not valid at the moment
+        valid_mirrors = [
+            m for m in mirrors if m["completion_pct"] > 0 and m["score"] is not None
+        ]
+
+        # Convert completion_pct in the mirror info to an actual percent
+        for m in valid_mirrors:
+            m["completion_pct"] *= 100
+
+    except OSError as err:
+        st.error(str(err))
+
+    return valid_mirrors
