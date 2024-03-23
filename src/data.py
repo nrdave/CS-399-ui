e747d784c1eb39011b7d36fce220ae02cbaa0973
Nikhil Dave <nikhilrdave2003@gmail.com>
2024-03-22 20:44:10 -0700

    Added exception raising to data collection
    Probably need to make it cleaner, but it's a start

--- src/data.py
+++ src/data.py
@@ -1,16 +1,20 @@
 import streamlit as st
 from requests import get
+from urllib3.exceptions import NameResolutionError
 
 
 # Arch Linux mirror checks generally run around every 7 minutes
 @st.cache_data(show_spinner="Fetching data from archlinux.org", ttl=60 * 7)
-def get_mirror_info():
+def get_mirror_info() -> list:
     """Fetch current mirror data from archlinux.org/mirrors/status/json"""
     MIRROR_STATUS_URL = "https://archlinux.org/mirrors/status/json/"
 
     try:
         # Get mirror info
         mirror_status = get(MIRROR_STATUS_URL, timeout=5).json()
+
+        if not mirror_status:
+            raise ConnectionError("Unable to connect to archlinux.org")
 
         # Extract information about each mirror
         mirrors = mirror_status.get("urls")
@@ -25,7 +29,14 @@ def get_mirror_info():
         for m in valid_mirrors:
             m["completion_pct"] *= 100
 
+        return valid_mirrors
+
+    except (ConnectionError, NameResolutionError) as err:
+        st.error(str(err))
+        raise err
+
     except OSError as err:
         st.error(str(err))
+        raise err
 
-    return valid_mirrors
+    return []
