b94f532b7b1928b1117f4141144f7c80bfabf659
Nikhil Dave <nikhilrdave2003@gmail.com>
2024-03-23 08:51:41 -0700

    Formatting fixes in the Mirror Inspector

--- src/pages/Mirror_Inspector.py
+++ src/pages/Mirror_Inspector.py
@@ -7,16 +7,21 @@ def main():
     mirror_data = data.get_mirror_info()
 
     country_set = sorted({m["country"] for m in mirror_data})
-    selected_countries = st.multiselect(
-        "Select Mirror Country/Countries: ", list(country_set)
-    )
+    selected_countries = st.selectbox(
+        "Select Mirror Country: ", list(country_set))
 
     valid_mirrors = [
         m for m in mirror_data if m["country"] in selected_countries]
 
+    # Had to disable my formatter for this to get it to fit in 79 characters
+    # fmt: off
     selected_mirror = st.selectbox(
-        "Select a mirror: ", valid_mirrors, format_func=lambda m: m["url"], index=None
+        "Select a mirror: ",
+        valid_mirrors,
+        format_func=lambda m: m["url"],
+        index=None
     )
+    # fmt: on
 
     if selected_mirror:
         st.json(selected_mirror)
