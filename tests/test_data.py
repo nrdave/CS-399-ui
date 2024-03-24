8c8c60f8344370935cde3791234df2a4103c90f4
Nikhil Dave <nikhilrdave2003@gmail.com>
2024-03-24 11:56:24 -0700

    Added tests to verify the format of the incoming data
    Useful for detecting changes in the API

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,6 @@
-from data import get_last_update, normalize_times
+from data import get_mirror_info, get_last_update, normalize_times
 import pytest
+from requests import get
 
 
 @pytest.fixture
@@ -77,3 +78,46 @@ def test_normalize_times(
     assert normalize_times(minute_delays) == (
         normalized_minute_delays, 'minutes')
     assert normalize_times(hour_delays) == (normalized_hour_delays, 'hours')
+
+
+@pytest.fixture
+def mirror_status_fields():
+    """Get the data fields in the Mirror Status file from archlinux.org"""
+    return ["cutoff", "last_check", "num_checks",
+            "check_frequency", "urls", "version"]
+
+
+@pytest.fixture
+def mirror_info_fields():
+    """Get the data fields for each mirror in the Mirror Status file"""
+    return ["url", "protocol", "last_sync", "completion_pct", "delay",
+            "duration_avg", "duration_stddev", "score", "active", "country",
+            "country_code", "isos", "ipv4", "ipv6", "details"]
+
+
+def test_mirror_data(mirror_status_fields, mirror_info_fields):
+    """
+    Check that the JSON fields from the Arch Linux Mirror Status website match
+    what the program expects"""
+    try:
+        MIRROR_STATUS_URL = "https://archlinux.org/mirrors/status/json/"
+
+        # Get mirror status manually, as get_mirror_info returns just
+        # the statistics for each mirror
+        mirror_status = get(MIRROR_STATUS_URL, timeout=5).json()
+
+        assert list(mirror_status.keys()) == mirror_status_fields
+
+        # Check the data version number - another check for possible
+        # breaking changes in the API
+        assert mirror_status["version"] == 3
+
+        # Check that each mirror contains the fields the program expects
+        # it to
+        mirrors = get_mirror_info()
+
+        for m in mirrors:
+            assert list(m.keys()) == mirror_info_fields
+
+    except ConnectionError:
+        return
